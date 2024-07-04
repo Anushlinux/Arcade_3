d51d94f9d16a0f41df574dce2cbbc39068905dbc
Anushlinux <anushrutpandit@gmail.com>
2024-07-04 15:05:33 +0530

    Messages

---
+++ arcade_3/src/components/Message.js
@@ -0,0 +1,68 @@
+import { useContext } from 'react'
+import UserContext from '~/lib/UserContext'
+import { deleteMessage } from '~/lib/Store'
+import TrashIcon from '~/components/TrashIcon'
+
+const Message = ({ message }) => {
+  const { user, userRoles } = useContext(UserContext)
+  const TrashIcon = (props) => {
+  const { size = 16 } = props
+
+  const MessageInput = ({ onSubmit }) => {
+  const [messageText, setMessageText] = useState('')
+
+  const submitOnEnter = (event) => {
+    // Watch for enter key
+    if (event.keyCode === 13) {
+      onSubmit(messageText)
+      setMessageText('')
+    }
+  }
+
+  return (
+    <div className="py-1 flex items-center space-x-2">
+      <div className="text-gray-100 w-4">
+        {(user?.id === message.user_id ||
+          userRoles.some((role) => ['admin', 'moderator'].includes(role))) && (
+          <button onClick={() => deleteMessage(message.id)}>
+            <TrashIcon />
+          </button>
+        )}
+      </div>
+      <div>
+        <p className="text-blue-700 font-bold">{message.author.username}</p>
+        <p className="text-white">{message.message}</p>
+      </div>
+       <input
+        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        type="text"
+        placeholder="Send a message"
+        value={messageText}
+        onChange={(e) => setMessageText(e.target.value)}
+        onKeyDown={(e) => submitOnEnter(e)}
+      />
+      <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="feather feather-trash-2"
+    >
+      <polyline points="3 6 5 6 21 6"></polyline>
+      <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+      <line x1="10" y1="11" x2="10" y2="17"></line>
+      <line x1="14" y1="11" x2="14" y2="17"></line>
+    </svg>
+  
+    </div>
+   )
+  }
+  }
+}
+
+export default Message
